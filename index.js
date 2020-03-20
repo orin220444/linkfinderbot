ccdc9a20e3e23f868db465664302f836e18dc19d
Orin Serimon <orin220444@protonmail.com>
2020-03-21 00:42:58 +0700

    delete debug trash

--- index.js
+++ index.js
@@ -5,25 +5,17 @@ bot.on('message', async(ctx) => {
     const message = ctx.message.message_id
     if(ctx.message.entities) {
     const entities = ctx.message.entities
-    console.log(entities)
     let likentity = entities.filter(item => item.type == 'text_link')
     const multiplylinks = Array.isArray(likentity)
     if(multiplylinks){
         let i = 0
         let url = ' '
-        //console.log(url)
         while (i < likentity.length) {
-            //console.log(url)
             url = likentity[i].url + "\n"
-            //console.log(url)
             i++
-            console.log(url)
             await ctx.reply(url)
         }
-        console.log(url)
-
         //await ctx.telegram.deleteMessage(message)
-
     }
     else if (likentity == undefined) {
         ctx.reply('Здесь нет ссылок!'/*, {reply_to_message_id: message}*/)
