55e4dc5a8e915e2de253bd7e6f93eecb80fc3f32
Orin Serimon <orin220444@protonmail.com>
2020-03-20 22:08:50 +0700

    message id in variable

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const Telegraf = require('telegraf')
 require('dotenv').config({path: './.env'});
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.on('message',(ctx) => {
+    const message = ctx.message.message_id
     if(ctx.message.entities) {
     const entities = ctx.message.entities
     console.log(entities)
     let likentity = entities.find(item => item.type == 'text_link')
     console.log(likentity)
     if (likentity == undefined) {
-        ctx.reply('Здесь нет ссылок!', {reply_to_message_id: ctx.message.message_id})
+        ctx.reply('Здесь нет ссылок!', {reply_to_message_id: message})
     }
     else {url = likentity.url
     ctx.reply(url)
-   ctx.telegram.deleteMessage(ctx.message.message_id)
+   ctx.telegram.deleteMessage(message)
     }}
 })
 bot.launch();
