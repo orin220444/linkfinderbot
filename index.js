336cedc58893146e04cbc03a17f5ffaebee8a238
Orin Serimon <orin220444@protonmail.com>
2020-03-21 00:57:01 +0700

    format the code using eslint

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
 require('dotenv').config({path: './.env'});
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.on('message', async(ctx) => {
-    console.log(ctx.message)
-    const message = ctx.message.message_id
-    if(ctx.message.entities) {
-    const entities = ctx.message.entities
-    let likentity = entities.filter(item => item.type == 'text_link')
-    if(likentity){
-    const multiplylinks = Array.isArray(likentity)
-    if(multiplylinks){
-        let i = 0
-        let url = ' '
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.on('message', async (ctx) => {
+  console.log(ctx.message);
+  if (ctx.message.entities) {
+    const entities = ctx.message.entities;
+    const likentity = entities.filter((item) => item.type == 'text_link');
+    if (likentity) {
+      const multiplylinks = Array.isArray(likentity);
+      if (multiplylinks) {
+        let i = 0;
+        let url = ' ';
         while (i < likentity.length) {
-            url = likentity[i].url + "\n"
-            i++
-            await ctx.reply(url)
+          url = likentity[i].url + '\n';
+          i++;
+          await ctx.reply(url);
         }
-        //await ctx.telegram.deleteMessage(message)
+        // await ctx.telegram.deleteMessage(message)
+      } else if (likentity == undefined) {
+        ctx.reply('Здесь нет ссылок!'/* , {reply_to_message_id: message}*/);
+      } else {
+        url = likentity.url;
+        await ctx.reply(url);
+        // await ctx.telegram.deleteMessage(message)
+      }
     }
-    else if (likentity == undefined) {
-        ctx.reply('Здесь нет ссылок!'/*, {reply_to_message_id: message}*/)
-    }
-    else {
-        url = likentity.url
-        await ctx.reply(url)
-   //await ctx.telegram.deleteMessage(message)
-    }
-}}
-})
+  }
+});
 bot.launch();
 // TODO: caption entities
 // TODO: just links
