32336d14a9fba259d96df6891b0bc626cf29a11c
Orin Serimon <orin220444@protonmail.com>
2020-03-20 22:19:11 +0700

    add async/await

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Telegraf = require('telegraf')
 require('dotenv').config({path: './.env'});
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.on('message',(ctx) => {
+bot.on('message', async(ctx) => {
     const message = ctx.message.message_id
     if(ctx.message.entities) {
     const entities = ctx.message.entities
@@ -11,9 +11,10 @@ bot.on('message',(ctx) => {
     if (likentity == undefined) {
         ctx.reply('Здесь нет ссылок!', {reply_to_message_id: message})
     }
-    else {url = likentity.url
-    ctx.reply(url)
-   ctx.telegram.deleteMessage(message)
+    else {
+        url = likentity.url
+    await ctx.reply(url)
+   await ctx.telegram.deleteMessage(message)
     }}
 })
 bot.launch();
