27f8485b4a202a498d129c8741b71a38118a730d
Orin Serimon <orin220444@protonmail.com>
2020-03-21 00:40:31 +0700

    add support of multilinks in one post

--- index.js
+++ index.js
@@ -7,14 +7,32 @@ bot.on('message', async(ctx) => {
     const entities = ctx.message.entities
     console.log(entities)
     let likentity = entities.filter(item => item.type == 'text_link')
-    console.log(likentity)
-    if (likentity == undefined) {
-        ctx.reply('Здесь нет ссылок!', {reply_to_message_id: message})
+    const multiplylinks = Array.isArray(likentity)
+    if(multiplylinks){
+        let i = 0
+        let url = ' '
+        //console.log(url)
+        while (i < likentity.length) {
+            //console.log(url)
+            url = likentity[i].url + "\n"
+            //console.log(url)
+            i++
+            console.log(url)
+            await ctx.reply(url)
+        }
+        console.log(url)
+
+        //await ctx.telegram.deleteMessage(message)
+
+    }
+    else if (likentity == undefined) {
+        ctx.reply('Здесь нет ссылок!'/*, {reply_to_message_id: message}*/)
     }
     else {
         url = likentity.url
-    await ctx.reply(url)
-   await ctx.telegram.deleteMessage(message)
-    }}
+        await ctx.reply(url)
+   //await ctx.telegram.deleteMessage(message)
+    }
+}
 })
 bot.launch();
