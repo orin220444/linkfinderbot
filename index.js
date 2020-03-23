deaac6ee63bf5094333105a4992d14b2b9898bd7
Orin Serimon <orin220444@protonmail.com>
2020-03-24 02:25:23 +0700

    finding links in posts with no hidden url

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ bot.on('message', async (ctx) => {
   if (ctx.message.entities) {
     const entities = ctx.message.entities;
     const likentity = entities.filter((item) => item.type == 'text_link');
+    const urlentity = entities.filter((item) => item.type == 'url');
     if (likentity) {
       const multiplylinks = Array.isArray(likentity);
       if (multiplylinks) {
@@ -25,8 +26,20 @@ bot.on('message', async (ctx) => {
         // await ctx.telegram.deleteMessage(message)
       }
     }
+    if(urlentity){
+      let i = 0;
+        let url = ' ';
+      const text = ctx.message.text
+      while (i < urlentity.length) {
+        start = urlentity[i].offset;
+        end = urlentity[i].offset + urlentity[i].length;       
+        url = text.substring(start, end)
+        await ctx.reply(url);
+        i++;
+      }
+    }
   }
 });
 bot.launch();
 // TODO: caption entities
-// TODO: just links
+// TODO: delete user messages
