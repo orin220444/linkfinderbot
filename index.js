0bbf3481799bfc214d16f8e7df52f21d0f144ad3
Orin Serimon <orin220444@protonmail.com>
2020-04-23 16:53:34 +0700

    some fixes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Telegraf = require('telegraf');
 require('dotenv').config({path: './.env'});
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.on('message', async (ctx) => {
+  console.log(ctx.message)
+  const message = ctx.message.message_id
   if (ctx.message.entities) {
     const entities = ctx.message.entities;
     const likentity = entities.filter((item) => item.type == 'text_link');
@@ -12,7 +14,7 @@ bot.on('message', async (ctx) => {
       while (i < likentity.length) {
         url = likentity[i].url;
         i++;
-        await ctx.reply(url);
+        await ctx.reply(url, {reply_to_message_id: message});
       }
     }
     // await ctx.telegram.deleteMessage(message)
@@ -24,7 +26,7 @@ bot.on('message', async (ctx) => {
         start = urlentity[i].offset;
         end = urlentity[i].offset + urlentity[i].length;
         url = text.substring(start, end);
-        await ctx.reply(url);
+        await ctx.reply(url, {reply_to_message_id: message});
         i++;
       }
     } else {
